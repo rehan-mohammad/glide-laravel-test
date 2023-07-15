327980a620c7279e08aeecf0fd3406a4b1183497
rehan <rehan@barques.co.uk>
2023-07-15 17:30:44 +0100

    Added functionality to account for randomised MAC addresses, and also outputting queries that return no results

--- app/Http/Controllers/OuiDataController.php
+++ app/Http/Controllers/OuiDataController.php
@@ -11,21 +11,31 @@ class OuiDataController extends Controller
     public function SingleMACLookup($query)
     {
 
-        //Remove all characters from queried MAC address except for numbers and letters
-        $filteredMAC = preg_replace("/[^a-zA-Z0-9]+/", '', $query);
+        //Remove all characters from queried MAC address except for numbers and letters, and also convert to uppercase
+        $filteredMAC = strtoupper(preg_replace("/[^a-zA-Z0-9]+/", '', $query));
+
+        //Check if second character indicates that it could be randomised
+        $isRandom = 0;
+        if(in_array(substr($filteredMAC, 1, 1), ['2','6','A','E'])){
+            $isRandom = 1;
+        }
 
         //Get first 6 characters from MAC address to get the OUI
         $OUIvalue = substr($filteredMAC, 0, 6);
 
         //Search database for OUI data matching the mac address queried
-        $OuiResults = OuiData::where('assignment', $OUIvalue)->get();
+        $ouiResult = OuiData::where('assignment', $OUIvalue)->first();
 
         //Create an object containing the queried mac address and the vendor name to output to the end user, matching the format in the brief.
         $OutputResults = array();
 
-        foreach ($OuiResults as $ouiResult){
-            $OutputResults[] = ['mac_address' => $query, 'vendor' => $ouiResult->organization_name];
-        }
+            if($isRandom){
+                //Add a message noting that this MAC address is possibly randomised
+                $OutputResults[] = ['mac_address' => $query, 'vendor' => $ouiResult->organization_name ?? 'No matching result', 'notes' => "The second character of this MAC Address indicates this could possibly be randomised"];
+            }
+            else {
+                $OutputResults[] = ['mac_address' => $query, 'vendor' => $ouiResult->organization_name ?? 'No matching result'];
+            }
 
         return json_encode($OutputResults);
     }
@@ -39,19 +49,29 @@ class OuiDataController extends Controller
 
         foreach ($requestArray as $query){
 
-            //Remove all characters from queried MAC address except for numbers and letters
-            $filteredMAC = preg_replace("/[^a-zA-Z0-9]+/", '', $query);
+            //Remove all characters from queried MAC address except for numbers and letters, and also convert to uppercase
+            $filteredMAC = strtoupper(preg_replace("/[^a-zA-Z0-9]+/", '', $query));
+
+            //Check if second character indicates that it could be randomised
+            $isRandom = 0;
+            if(in_array(substr($filteredMAC, 1, 1), ['2','6','A','E'])){
+                $isRandom = 1;
+            }
 
             //Get first 6 characters from MAC address to get the OUI
             $OUIvalue = substr($filteredMAC, 0, 6);
 
             //Search database for OUI data matching the mac address queried
-            $OuiResults = OuiData::where('assignment', $OUIvalue)->get();
+            $ouiResult = OuiData::where('assignment', $OUIvalue)->first();
 
-            foreach ($OuiResults as $ouiResult){
                 //Create an object containing the queried mac address and the vendor name to output to the end user, matching the format in the brief.
-                $OutputResults[] = ['mac_address' => $query, 'vendor' => $ouiResult->organization_name];
-            }
+                if($isRandom){
+                    //Add a message noting that this MAC address is possibly randomised
+                    $OutputResults[] = ['mac_address' => $query, 'vendor' => $ouiResult->organization_name ?? 'No matching result', 'notes' => "The second character of this MAC Address indicates this could possibly be randomised"];
+                }
+                else {
+                    $OutputResults[] = ['mac_address' => $query, 'vendor' => $ouiResult->organization_name ?? 'No matching result'];
+                }
 
         }
 
